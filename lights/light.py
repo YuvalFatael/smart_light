40bd7dc4618ba98f50fbcaad7cb12da1cf8286c5
Yuval <yuvalf@gmail.com>
2017-05-03 22:21:01 +0300

    Inital commit

---
+++ lights/light.py
@@ -0,0 +1,29 @@
+# Import SDK packages
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+
+def customCallback(client, userdata, message):
+	print("Received a new message: ")
+	print(message.payload)
+	print("from topic: ")
+	print(message.topic)
+	print("--------------\n\n")
+
+# For certificate based connection
+myMQTTClient = AWSIoTMQTTClient("light")
+# For TLS mutual authentication
+myMQTTClient.configureEndpoint("audsodu4ke8z4.iot.us-west-2.amazonaws.com", 8883)
+myMQTTClient.configureCredentials("certs/root-CA.crt", "certs/private.key", "certs/cert.pem")
+#myMQTTClient.configureCredentials("YOUR/ROOT/CA/PATH", "PRIVATE/KEY/PATH", "CERTIFICATE/PATH")
+#myMQTTClient.configureCredentials("root-CA.crt")
+
+myMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+myMQTTClient.connect()
+# myMQTTClient.subscribe("test", 1, customCallback)
+myMQTTClient.publish("test", "hey this is a message", 0)
+# myMQTTClient.unsubscribe("test")
+myMQTTClient.disconnect()
+
