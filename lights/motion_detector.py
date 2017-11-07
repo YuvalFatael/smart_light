3334938240d879424c4472c247c188bfefaecb22
Yuval Fatael <yuvalf@gmail.com>
2017-11-07 20:05:24 +0200

    motion_detector.py: speed float foramtting

--- lights/motion_detector.py
+++ lights/motion_detector.py
@@ -320,7 +320,8 @@ def md(path_to_video, cb):
 				image_filename_path = os.path.join('images', image_filename + '-{}.jpg'.format(direction))
 				cv2.imwrite(image_filename_path, frame2show)
 
-			threading.Thread(target=cb, args=[direction, speedMeterPerSecond, image_filename_path]).start()
+			speedMeterPerSecond = float('{:.3f}'.format(speedMeterPerSecond)
+			threading.Thread(target=cb, args=[direction, speedMeterPerSecond), image_filename_path]).start()
 
 		algo_end_time = time.time()
 		algo_time = algo_end_time - start_time
